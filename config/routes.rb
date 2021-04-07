72ab7640a561770fb5c2a739fce371f44d56cc2b
minami0601 <minami.love.bb@icloud.com>
2021-04-07 18:05:02 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+  resources :users, only: %i(index) 
+
+
+  resources :posts, only: %i(new create index show destroy) do
+    resources :photos, only: %i(create)
+  end
+end
